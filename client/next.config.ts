621eb42ba5d8a9397cdeb3cae5a5c9f564688c2b
Ilyass Az <ilyassazetd@gmail.com>
2025-06-19 20:36:35 +0100

    initial commit

---
+++ client/next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
